916fc836318acc527b12e3500e09c83564d60488
Grace Akpa <melianakpa@gmail.com>
2022-12-12 11:10:49 +0000

    Ajout EJB CustomerManager

---
+++ src/main/java/ma/emsi/tpcustomermeliane/config/configJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ma.emsi.tpcustomermeliane.config;
+
+/**
+ *
+ * @author GRACE_AKPA
+ */
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+@ApplicationScoped
+@FacesConfig
+public class configJSF {
+    
+}
---
+++ src/main/java/ma/emsi/tpcustomermeliane/jsf/CustomerBean.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package ma.emsi.tpcustomermeliane.jsf;
+
+import jakarta.ejb.EJB;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+import ma.emsi.tpcustomermeliane.Customer;
+import ma.emsi.tpcustomermeliane.ejb.CustomerManager;
+
+/**
+ *
+ * @author GRACE_AKPA
+ */
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable {
+    private List<Customer> customerList;  
+
+  @EJB
+  private CustomerManager customerManager;  
+        
+  public CustomerBean() {  }  
+        
+  /** 
+   * Retourne la liste des clients pour affichage dans une DataTable 
+   * @return 
+   */  
+  public List<Customer> getCustomers() {
+    if (customerList == null) {
+      customerList = customerManager.getAllCustomers();
+    }
+    return customerList;
+  }  
+
+}
